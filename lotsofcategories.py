56922f05555c9626954a98313df9b3a776cd97a5
amharF <amhar.ford@gmail.com>
2015-10-31 20:31:33 +0100

    recreated database stored grocery item images in /static folder made minor layout changes to some .html templates

--- lotsofcategories.py
+++ lotsofcategories.py
@@ -20,7 +20,7 @@ session = DBSession()
 
 # Create dummy user
 User1 = User(name="Amhar Ford", email="amhar.ford@gmail.com",
-             picture='https://lh5.googleusercontent.com/-fEdEA2m4s20/AAAAAAAAAAI/AAAAAAAAEXY/wJ66AgeAHLQ/photo.jpg')
+             picture='http://www.netentstalker.com/wp-content/uploads/2014/02/Wonky-Wabbits-Rabbit.jpg')
 session.add(User1)
 session.commit()
 
@@ -35,18 +35,14 @@ groceryItem1 = GroceryItem(name = "cheese", description = "Cheese is a food \
 			derived from milk that is produced in a wide range of flavors, \
 			textures, and forms by coagulation of the milk protein casein. It\
 			 comprises proteins and fat from cow's milk.", price = "$3.20", 
-			item_image = "http://www.automationheroes.com/wp-content/uploads/\
-			2013/11/tumblr_static_cheese_205_1362800142.jpg",
-			category = category1)
+			item_image = "/static/cheese.jpg", category = category1)
 
 session.add(groceryItem1)
 session.commit()
 
 groceryItem2 = GroceryItem(name = "milk", description = "Milk is a white \
 			liquid produced by the mammary glands of cows.", price = "$0.90", 
-			item_image = "http://www.wainwrightdairy.com/wp-content/uploads/\
-			2015/04/milk2.jpg", 
-			category = category1)
+			item_image = "/static/milk.jpg", category = category1)
 
 session.add(groceryItem2)
 session.commit()
@@ -54,9 +50,7 @@ session.commit()
 groceryItem3 = GroceryItem(name = "yoghurt", description = "Yoghurt is a food\
 			 produced by bacterial fermentation of milk. The bacteria used to\
 			  make yogurt are known as \'yogurt cultures\'.", price = "$1.40",
-			item_image = "http://www.womenshealthmag.com/sites/\
-			womenshealthmag.com/files/images/chobani-yogurt_0.jpg",
-			category = category1)
+			item_image = "/static/yoghurt.jpg", category = category1)
 
 session.add(groceryItem3)
 session.commit()
@@ -64,8 +58,7 @@ session.commit()
 groceryItem4 = GroceryItem(name = "cream", description = "Cream is a dairy \
 			product composed of the higher-butterfat layer skimmed from the \
 			top of milk before homogenization.", price = "$1.60", 
-			item_image ="http://www.coldstonecreamery.com/assets/img/products\
-			/mixins/whippedcream.jpg", category = category1)
+			item_image ="/static/cream.jpg", category = category1)
 
 session.add(groceryItem4)
 session.commit()
@@ -73,8 +66,7 @@ session.commit()
 groceryItem5 = GroceryItem(name = "butter", description = "Butter is a solid \
 			dairy product made by churning fresh or fermented cream or milk, \
 			to separate the butterfat from the buttermilk.", price = "$3.20", 
-			item_image = "https://upload.wikimedia.org/wikipedia/commons/f/\
-			fd/Western-pack-butter.jpg", category = category1)
+			item_image = "/static/butter.jpg", category = category1)
 
 session.add(groceryItem5)
 session.commit()
@@ -89,9 +81,8 @@ session.commit()
 groceryItem1 = GroceryItem(name = "potatoes", description = "The potato is a \
 			starchy, tuberous crop from the perennial nightshade Solanum \
 			tuberosum L. The word \'potato\' may refer either to the plant \
-			itself or to the edible tuber.", price = "$3.00", item_image = "\
-			https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg",
-			category = category2)
+			itself or to the edible tuber.", price = "$3.00", 
+			item_image = "/static/potato.jpg", category = category2)
 
 session.add(groceryItem1)
 session.commit()
@@ -99,8 +90,8 @@ session.commit()
 groceryItem2 = GroceryItem(name = "carrots", description = "The carrot is a \
 			root vegetable, usually orange in colour, though purple, red, \
 			white, and yellow varieties exist. It has a crisp texture when \
-			fresh.", price = "$1.80", item_image = "http://media.mercola.com/\
-			assets/images/food-facts/carrot-fb.jpg", category = category2)
+			fresh.", price = "$1.80", item_image = "/static/carrots.jpg", 
+			category = category2)
 
 session.add(groceryItem2)
 session.commit()
@@ -108,8 +99,7 @@ session.commit()
 groceryItem3 = GroceryItem(name = "lettuce", description = "Lettuce is an \
 			annual plant of the daisy family Asteraceae. It is most often \
 			grown as a leaf vegetable, but sometimes for its stem and seeds.",
-			price = "$0.80", item_image = "http://16acresgardencenter.com/\
-			wp-content/uploads/2014/06/Lettuce.jpg",
+			price = "$0.80", item_image = "/static/lettuce.jpg",
 			category = category2)
 
 session.add(groceryItem3)
@@ -118,9 +108,8 @@ session.commit()
 groceryItem4 = GroceryItem(name = "cucumber", description = "Cucumber is a \
 			widely cultivated plant in the gourd family, Cucurbitaceae. \
 			It is a creeping vine that bears cylindrical fruits that are used\
-			 as culinary vegetables.", price = "$0.90", item_image ="http://\
-			i.livescience.com/images/i/000/076/219/iFF/cucumbers.jpg",
-			category = category2)
+			 as culinary vegetables.", price = "$0.90", 
+			 item_image ="/static/cucumber.jpg", category = category2)
 
 session.add(groceryItem4)
 session.commit()
@@ -128,9 +117,8 @@ session.commit()
 groceryItem5 = GroceryItem(name = "onions", description = "The onion, also \
 			known as the bulb onion or common onion, is a vegetable and is \
 			the most widely cultivated species of the genus Allium.", 
-			price = "$3.20", item_image = "https://upload.wikimedia.org/\
-			wikipedia/commons/f/fd/Western-pack-butter.jpg",
-			category = category1)
+			price = "$3.20", item_image = "/static/onion.jpg",
+			category = category2)
 
 session.add(groceryItem5)
 session.commit()
@@ -146,8 +134,8 @@ groceryItem1 = GroceryItem(name = "apples", description = "The apple tree is \
 			a deciduous tree in the rose family best known for its sweet, \
 			pomaceous fruit, the apple. It is cultivated worldwide as a fruit\
 			 tree, and is the most widely grown species in the genus Malus.", 
-			price = "$3.10", item_image = "http://media.mercola.com/assets/\
-			images/food-facts/apple.jpg", category = category3)
+			price = "$3.10", item_image = "/static/apple.jpg", 
+			category = category3)
 
 session.add(groceryItem1)
 session.commit()
@@ -156,8 +144,8 @@ groceryItem2 = GroceryItem(name = "bananas", description = "The banana is an \
 			edible fruit, botanically a berry, produced by several kinds of \
 			large herbaceous flowering plants in the genus Musa. In some \
 			countries, bananas used for cooking may be called plantains.",
-			price = "$1.60", item_image = "https://www.organicfacts.net/\
-			wp-content/uploads/2013/05/Banana21.jpg", category = category3)
+			price = "$1.60", item_image = "/static/banaba.jpg", 
+			category = category3)
 
 session.add(groceryItem2)
 session.commit()
@@ -166,9 +154,8 @@ groceryItem3 = GroceryItem(name = "oranges", description = "The orange is the\
 			 fruit of the citrus species Citrus x sinensis in the family \
 			Rutaceae. The fruit of the Citrus x sinensis is considered a swee\
 			t orange, whereas the fruit of the Citrus x aurantium is consider\
-			ed a bitter orange.", price = "$2.60", item_image = "https://uplo\
-			ad.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-&-Split.jpg",
-			category = category3)
+			ed a bitter orange.", price = "$2.60", 
+			item_image = "/static/orange.jpg", category = category3)
 
 session.add(groceryItem3)
 session.commit()
@@ -178,8 +165,7 @@ groceryItem4 = GroceryItem(name = "lemons", description = "The lemon is a \
 			ellipsoidal yellow fruit is used for culinary and non-culinary \
 			purposes throughout the world, primarily for its juice, which has\
 			 both culinary and cleaning uses.", price = "$1.70", item_image =
-			"https://upload.wikimedia.org/wikipedia/commons/c/c7/Lemon-Whole-\
-			Split.jpg", category = category3)
+			"/static/lemon.jpg", category = category3)
 
 session.add(groceryItem4)
 session.commit()
@@ -187,8 +173,7 @@ session.commit()
 groceryItem5 = GroceryItem(name = "pears", description = "The pear is any of \
 			several tree and shrub species of genus Pyrus in the family Rosac\
 			eae. It is also the name of the pomaceous fruit of these trees.", 
-			price = "$2.40", item_image = "http://images2.wikia.nocookie.net/\
-			__cb20110210142733/recipes/images/d/db/Packham_pear.jpg", 
+			price = "$2.40", item_image = "/static/pear.jpg", 
 			category = category3)
 
 session.add(groceryItem5)
@@ -204,9 +189,8 @@ session.commit()
 groceryItem1 = GroceryItem(name = "pork chops", description = "A pork chop is\
 			 a chop of pork cut perpendicularly to the spine of the pig and \
 			usually containing a rib or part of a vertebra, served as an \
-			individual portion.", price = "$3.80", item_image = "https://www.\
-			hiddenvalley.com/cache/0f1bb4c47aabfa3e5c306bddd2a2f5f1.jpg", 
-			category = category4)
+			individual portion.", price = "$3.80", 
+			item_image = "/static/porkchop.jpg", category = category4)
 
 session.add(groceryItem1)
 session.commit()
@@ -214,18 +198,16 @@ session.commit()
 groceryItem2 = GroceryItem(name = "lamb chops", description = "A lamb chop is\
 			 a cut of meat from a lamb,cut perpendicularly to the spine, and \
 			usually containing a rib or riblet part of a vertebra and served \
-			as an individual portion.", price = "$5.60", item_image = "http:\
-			//blog.sanuraweathers.com/wp-content/uploads/2012/03/LambChops_61\
-			51.jpg", category = category4)
+			as an individual portion.", price = "$5.60", 
+			item_image = "/static/lambchop.jpg", category = category4)
 
 session.add(groceryItem2)
 session.commit()
 
 groceryItem3 = GroceryItem(name = "beef steak", description = "A steak is a \
 			meat generally sliced perpendicular to the muscle fibers, \
-			potentially including a bone.", price = "$6.70", item_image = 
-			"http://ubons.net/wp-content/uploads/2012/01/Beef-steak.jpg",
-			category = category4)
+			potentially including a bone.", price = "$6.70", 
+			item_image = "/static/steak.jpg", category = category4)
 
 session.add(groceryItem3)
 session.commit()
@@ -236,8 +218,8 @@ groceryItem4 = GroceryItem(name = "whole chicken", description = "The chicken\
 			population of more than 24 billion in 2003,[1] there are more \
 			chickens in the world than any other species of bird. Humans keep\
 			 chickens primarily as a source of food, consuming both their mea\
-			 t and their eggs.", price = "$8.90", item_image = "http://www.th\
-			 ebutchersblocksc.com/chicken_001.jpg", category = category4)
+			 t and their eggs.", price = "$8.90", 
+			 item_image = "/static/chicken.jpg", category = category4)
 
 session.add(groceryItem4)
 session.commit()
@@ -246,8 +228,7 @@ groceryItem5 = GroceryItem(name = "beef burgers", description = "A patty, in \
 			American, Canadian, South African, Australian and New Zealand \
 			English, is a flattened, usually round, serving of ground beef. \
 			The meat is compacted and shaped, cooked, and served.", price = 
-			"$2.50", item_image = "https://upload.wikimedia.org/wikipedia/\
-			commons/d/d2/Pre-formed_hamburger.JPG", category = category4)
+			"$2.50", item_image = "/static/burger.jpg", category = category4)
 
 session.add(groceryItem5)
 session.commit()
@@ -258,13 +239,12 @@ category5 = Category(name = "Bakery")
 session.add(category5)
 session.commit()
 
-groceryItem1 = GroceryItem(name = "wholemeal bread", description = "Whole-\
+groceryItem1 = GroceryItem(name = "wholewheat bread", description = "Whole-\
 			wheat bread or wholemeal bread is a type of bread made using \
 			flour that is partly or entirely milled from whole or almost-\
 			whole wheat grains, see whole-wheat flour and whole grain. It is \
-			one kind of brown bread.", price = "$2.40", item_image = "https:/\
-			/upload.wikimedia.org/wikipedia/commons/4/46/Vegan_no-knead_whole\
-			_wheat_bread_loaf,_September_2010.jpg", category = category5)
+			one kind of brown bread.", price = "$2.40", 
+			item_image = "/static/wholewheatbread.jpg", category = category5)
 
 session.add(groceryItem1)
 session.commit()
@@ -272,9 +252,7 @@ session.commit()
 groceryItem2 = GroceryItem(name = "corn bread", description = "Cornbread is a\
 			 generic name for any number of quick breads containing cornmeal.\
 			  They are usually leavened by baking powder.", price = "$2.20", 
-			item_image = "http://wickedgoodkitchen.com/wp-content/uploads/\
-			2013/11/Grandmas-Sweet-Buttermilk-Cornbread-4.jpg", 
-			category = category5)
+			item_image = "/static/cornbread.jpg", category = category5)
 
 session.add(groceryItem2)
 session.commit()
@@ -283,9 +261,8 @@ groceryItem3 = GroceryItem(name = "white bread", description = "White bread \
 			typically refers to breads made from wheat flour from which the \
 			bran and the germ layers have been removed from the whole \
 			wheatberry as part of the flour grinding or milling process, \
-			producing a light-colored flour.", price = "$1.80", item_image = 
-			"https://thenypost.files.wordpress.com/2014/05/whitebread.jpg",
-			category = category5)
+			producing a light-colored flour.", price = "$1.80", 
+			item_image = "/static/whitebread.jpg", category = category5)
 
 session.add(groceryItem3)
 session.commit()
@@ -295,9 +272,8 @@ groceryItem4 = GroceryItem(name = "hot cross bun", description = "A hot cross\
 			 ed with a cross on the top, traditionally eaten on Good Friday \
 			 in the United Kingdom, Ireland, Australia, New Zealand, South Afr\
 			 ica, Canada, India, and United States, and now available all year \
-			 round in some places.", price = "$2.00", item_image = "https://\
-			 foodimentaryguy.files.wordpress.com/2011/09/www-chatmagazine-co-\
-			 uk.jpg", category = category5)
+			 round in some places.", price = "$2.00", 
+			 item_image = "/static/hotcrossbun.jpg", category = category5)
 
 session.add(groceryItem4)
 session.commit()
@@ -306,8 +282,7 @@ groceryItem5 = GroceryItem(name = "croissant", description = "A croissant is \
 			a buttery, flaky, viennoiserie or Vienna-style pastry named for \
 			its well-known crescent shape. Croissants and other viennoiserie \
 			are made of a layered yeast-leavened dough.", price = "$1.00", 
- 			item_image = "http://www.creationfood.ca/wp-content/uploads/2015/\
- 			02/croissants-korea-AFPrelax-151113.jpg", category = category5)
+ 			item_image = "/static/croissant.jpg", category = category5)
 
 session.add(groceryItem5)
 session.commit()
@@ -320,17 +295,15 @@ session.commit()
 
 groceryItem1 = GroceryItem(name = "coffee", description = "Coffee is a brewed\
 			 drink prepared from roasted coffee beans, which are the seeds of\
-			  berries from the Coffea plant.", price = "$2.60", item_image = 
-			"http://images.wisegeek.com/cup-of-coffee-with-creamer-and-coffee\
-			-beans.jpg", category = category6)
+			  berries from the Coffea plant.", price = "$2.60", 
+			  item_image = "/static/coffee.jpg", category = category6)
 
 session.add(groceryItem1)
 session.commit()
 
 groceryItem2 = GroceryItem(name = "apple juice", description = "Apple juice i\
 			s a fruit juice made by the maceration and pressing of apples.", 
-			 price = "$1.40", item_image = "https://upload.wikimedia.org/wiki\
-			 pedia/commons/2/23/Apple_juice_with_3apples.jpg", 
+			 price = "$1.40", item_image = "/static/applejuice.jpg", 
 			 category = category6)
 
 session.add(groceryItem2)
@@ -340,8 +313,8 @@ groceryItem3 = GroceryItem(name = "tea", description = "Tea is an aromatic be\
 			verage commonly prepared by pouring hot or boiling water over cur\
 			ed leaves of the Camellia sinensis, an evergreen shrub native to \
 			Asia. After water, it is the most widely consumed drink in the wo\
-			rld.", price = "$1.40", item_image = "http://weknowyourdreams.com\
-			/image.php?pic=/images/tea/tea-07.jpg", category = category6)
+			rld.", price = "$1.40", item_image = "/static/tea.jpg", 
+			category = category6)
 
 session.add(groceryItem3)
 session.commit()
@@ -350,9 +323,8 @@ groceryItem4 = GroceryItem(name = "orange juice", description = "Orange juice\
 			 is the liquid extract of the fruit of the orange tree. It is mad\
 			 e by squeezing the fresh orange, drying and later re-hydrating t\
 			 he juice, or concentration of the juice and later adding water t\
-			 o the concentrate.", price = "$1.60", item_image = "http://autho\
-			 ritynutrition.com/wp-content/uploads/2013/10/orange-juice.jpg", 
-			category = category6)
+			 o the concentrate.", price = "$1.60", 
+			 item_image = "/static/orangejuice.jpg", category = category6)
 
 session.add(groceryItem4)
 session.commit()
@@ -361,8 +333,7 @@ groceryItem5 = GroceryItem(name = "cola", description = "Cola is a sweetened,\
 			 carbonated soft drink, derived from drinks that originally conta\
 			 ined caffeine from the kola nut and cocaine from coca leaves, fl\
 			 avored with vanilla and other ingredients.", price = "$1.80", 
-			 item_image = "http://www.wellbeing.com.au/uploads/news/Cola.jpg"
-			 , category = category6)
+			 item_image = "/static/cola.jpg", category = category6)
 
 session.add(groceryItem5)
 session.commit()
